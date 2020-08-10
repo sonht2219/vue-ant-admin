b2ecdba0210c22d941afc2ef2155668fbb09f794
sonht <sonht2219@gmail.com>
2020-08-10 14:07:30 +0700

    fix base

--- src/modules/auth/Register/index.vue
+++ src/modules/auth/Register/index.vue
@@ -1,60 +1,117 @@
 <template>
     <div class="main layout-register">
         <h3><span>Đăng ký</span></h3>
-        <a-form ref="formRegister" id="formRegister">
-            <a-form-item>
+        <a-form-model ref="formRegister"
+                      id="formRegister"
+                      :model="form"
+                      :rules="rules"
+        >
+            <a-form-model-item prop="email" has-feedback>
                 <a-input
                         size="large"
                         type="text"
                         placeholder="Email"
+                        v-model="form.email"
                 ></a-input>
-            </a-form-item>
+            </a-form-model-item>
 
-            <a-form-item>
+            <a-form-model-item prop="phoneNumber" has-feedback>
+                <a-input size="large"
+                         placeholder="Số điện thoại"
+                         v-model="form.phoneNumber"
+                ></a-input>
+            </a-form-model-item>
+
+            <a-form-model-item prop="password" has-feedback>
                 <a-input-password
                         size="large"
                         placeholder="Mật khẩu"
+                        v-model="form.password"
                 ></a-input-password>
-            </a-form-item>
+            </a-form-model-item>
 
-            <a-form-item>
+            <a-form-model-item prop="passwordConfirm" has-feedback>
                 <a-input-password
                         size="large"
                         placeholder="Xác nhận mật khẩu"
+                        v-model="form.passwordConfirm"
                 ></a-input-password>
-            </a-form-item>
+            </a-form-model-item>
 
-            <a-form-item>
-                <a-input size="large" placeholder="Số điện thoại">
-                    <a-select slot="addonBefore" size="large" defaultValue="+86">
-                        <a-select-option value="+86">+84</a-select-option>
-                    </a-select>
-                </a-input>
-            </a-form-item>
-
-            <a-form-item>
+            <a-form-model-item>
                 <a-button
                         size="large"
                         type="primary"
-                        htmlType="submit"
-                        class="register-button">
+                        class="register-button"
+                        @click="onSubmit"
+                >
                     Đăng ký
                 </a-button>
                 <router-link class="login" :to="{ name: 'login' }">Đăng nhập</router-link>
-            </a-form-item>
+            </a-form-model-item>
 
-        </a-form>
+        </a-form-model>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
+    import {FormModel} from "ant-design-vue";
+    import {PHONE_REGEX} from "@/shared/constant";
+    import {register} from "@/modules/auth/service";
 
     @Component({
         name: "Register"
     })
     export default class extends Vue {
+        private form = {
+            email: "",
+            phoneNumber: "",
+            password: "",
+            passwordConfirm: ""
+        };
 
+        private rules = {
+            email: [{ required: true, type: "email", message: "Nhập email", trigger: "change" }],
+            phoneNumber: [
+                { required: true, message: "Nhập số điện thoại", trigger: "change" },
+                { pattern: new RegExp(PHONE_REGEX), message: "Số điện thoại không hợp lệ", trigger: "blur" }
+            ],
+            password: [{ required: true, validator: this.passwordValidator, trigger: "change"}],
+            passwordConfirm: [{ required: true, validator: this.passwordConfirmValidator, trigger: "change"}]
+        };
+
+        private passwordValidator(rule: any, value: any, callback: Function) {
+            if (value === "") {
+                callback(new Error("Nhập mật khẩu"));
+            } else {
+                if (this.form.passwordConfirm !== "") {
+                    (this.$refs.formRegister as FormModel).validateField("passwordConfirm", () => 1);
+                }
+                callback();
+            }
+        }
+
+        private passwordConfirmValidator(rule: any, value: any, callback: Function) {
+            if (value === "") {
+                callback(new Error("Nhập lại mật khẩu"));
+            } else if (value !== this.form.password) {
+                callback(new Error("Mật khẩu không khớp"));
+            } else {
+                callback();
+            }
+        }
+
+        private onSubmit() {
+            (this.$refs.formRegister as FormModel).validate(async valid => {
+                if (!valid) return;
+                try {
+                    await register(this.form);
+                } catch (e) {
+                    console.log(e);
+                }
+            })
+        }
     }
 </script>
 
--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -1,1 +1,2 @@
 export const ACCESS_TOKEN = "sdjfhsdgwjfker123123nvmcnmvn";
+export const PHONE_REGEX = /(09[0|1|2|3|4|5|6|7|8|9]|03[2|3|4|5|6|7|8|9]|05[6|8|9]|07[0|6|7|8|9]|08[1|2|3|4|5|6|8|9])+([0-9]{7})\b/g;
